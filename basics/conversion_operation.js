d0ebc6b8b7d15b1a43d28cf744e0497a97d94240
dattugama1 <128592604+dattugama1@users.noreply.github.com>
2023-07-23 15:10:11 +0000

    conversion of datatypes

---
+++ basics/conversion_operation.js
@@ -0,0 +1,21 @@
+let score = true
+
+ //console.log(typeof score);
+ //console.log(typeof (score));
+
+let numberInvalue = Number(score)
+ //console.log(typeof numberInvalue);
+ //console.log(numberInvalue);
+
+
+let idLoggedIn = 0
+let boleeanLoggedIn =Boolean ( idLoggedIn )
+ //console.log(boleeanLoggedIn);
+ // 0 => false , 1 =>true
+ // "" =>false
+  // "dattu" => true
+
+  let number = 33
+  let numberInString =  String (number);
+  console.log(numberInString);
+  console.log(typeof numberInString);
