350806d1dfc333eaccb47f462a039c92eb6e0577
dattugama1 <128592604+dattugama1@users.noreply.github.com>
2023-07-23 14:21:57 +0000

    data_types

---
+++ basics/data_types.js
@@ -0,0 +1,22 @@
+// "use strict"; treat all js code aas newer version
+
+// alert(3 + 3); we are using nodejs not in browser
+
+ // datatype in javascript
+
+ //number => 2 topower 50
+//bigInt
+ //boolean
+ //null  standalone value
+ //undefined 
+ //symbol 
+
+
+
+let name = "dattu"
+let age = 21
+let isLoggedIn = true
+
+console.log(typeof age);
+console.log(typeof undefined);  // undefined
+console.log(typeof null);  //object
