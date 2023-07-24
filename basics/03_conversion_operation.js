d917dfb0935d490a5ded77a487253f700313ca02
dattugama1 <128592604+dattugama1@users.noreply.github.com>
2023-07-24 17:57:51 +0000

    conversion oppertaion in js

---
+++ basics/03_conversion_operation.js
@@ -0,0 +1,54 @@
+// let score = true
+
+//  //console.log(typeof score);
+//  //console.log(typeof (score));
+
+// let numberInvalue = Number(score)
+//  //console.log(typeof numberInvalue);
+//  //console.log(numberInvalue);
+
+
+// let idLoggedIn = 0
+// let boleeanLoggedIn =Boolean ( idLoggedIn )
+//  //console.log(boleeanLoggedIn);
+//  // 0 => false , 1 =>true
+//  // "" =>false
+//   // "dattu" => true
+
+  let number = 33
+  let numberInString =  String (number);
+   //console.log(numberInString);
+  //console.log(typeof numberInString);
+
+   // ***********OPERATIONS*************
+
+
+  let value = 3
+  let negValue = -value
+   //console.log(negValue);
+
+//    console.log(2 + 2);
+//    console.log(2 - 2);
+//    console.log(2 * 2);
+//    console.log(2 / 2);
+//    console.log(2 ** 2);
+//    console.log(2 % 3);
+
+let str1 = "hello "
+let str2 = "dattu"
+
+let str3 = (str1 + str2);
+// console.log(str3);
+// console.log(1+2);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+
+// console.log((3 + 4) * (5 % 3)); 
+//  console.log(+true);
+//  console.log(+"")
+let gameCounter = 100
+++gameCounter
+// ++gameCounter
+console.log(gameCounter);
+
