239bfcad227959609a75c2cb429b8620016f7763
dattugama1 <128592604+dattugama1@users.noreply.github.com>
2023-07-29 07:22:55 +0000

    switch javascripr

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march" 
+
+switch (month) {
+    case "january":
+        console.log("january")
+        break;
+    case "february":
+        console.log("februar")
+        break;
+    case "march":
+        console.log("march")
+        break;
+    case "april":
+        console.log("april")
+        break;
+
+    default:
+        console.log("this ia default match")
+        break;
+}
